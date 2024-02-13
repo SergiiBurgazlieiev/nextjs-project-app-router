c90f611d102d434a657e7b4b5954b6da45c78be1
Sergii Burgazlieiev <frontenddevsb@gmail.com>
2024-02-13 15:00:53 -0600

    init meals page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,22 @@
+import Link from 'next/link';
+
+import styles from './page.module.css';
+
 const MealsPage = () => {
 	return (
-		<main>
-			<h1>Meals Page</h1>
-		</main>
+		<>
+			<header className={styles.header}>
+				<h1>
+					Delicious meals, created
+					<span className={styles.highlight}>by you</span>
+				</h1>
+				<p>Choose your own recipe and cook it yourself.</p>
+				<p className={styles.cta}>
+					<Link href='/meals/share'>Share Your Favorite Recipe</Link>
+				</p>
+			</header>
+			<main className={styles.main}></main>
+		</>
 	);
 };
 
