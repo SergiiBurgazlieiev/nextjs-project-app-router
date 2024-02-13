8dba3a566a70d5152f7e8c4ba71fadc1213bf591
Sergii Burgazlieiev <frontenddevsb@gmail.com>
2024-02-13 17:47:33 -0600

    meal detail page

--- app/meals/[mealId]/page.js
+++ app/meals/[mealId]/page.js
@@ -1,9 +1,38 @@
-const MealDetailPage = () => {
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
+
+import styles from './page.module.css';
+import { getMeal } from '@/lib/meals';
+
+const MealDetailsPage = ({ params }) => {
+	const meal = getMeal(params.mealId);
+
+	if (!meal) {
+		notFound();
+	}
+	meal.instructions = meal.instructions.replace(/\n/g, '<br/>');
+
 	return (
-		<main>
-			<h1>Meal detail Page</h1>
-		</main>
+		<>
+			<header className={styles.header}>
+				<div className={styles.image}>
+					<Image src={meal.image} alt={meal.title} fill />
+				</div>
+				<div className={styles.headerText}>
+					<h1>{meal.title}</h1>
+					<p className={styles.creator}>
+						by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+					</p>
+					<p className={styles.summary}>{meal.summary}</p>
+				</div>
+			</header>
+			<main>
+				<p
+					className={styles.instructions}
+					dangerouslySetInnerHTML={{ __html: meal.instructions }}></p>
+			</main>
+		</>
 	);
 };
 
-export default MealDetailPage;
+export default MealDetailsPage;
