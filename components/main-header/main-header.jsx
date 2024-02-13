a6b266a71bade0f742514f4d193ac5aad38a2843
Sergii Burgazlieiev <frontenddevsb@gmail.com>
2024-02-13 12:27:44 -0600

    main header background component

--- components/main-header/main-header.jsx
+++ components/main-header/main-header.jsx
@@ -5,25 +5,29 @@ import Image from 'next/image';
 // Internal modules
 import logoImg from '@/assets/logo.png';
 import styles from './main-header.module.css';
+import MainHeaderBackground from '../main-header-background/main-header-background';
 
 const MainHeader = () => {
 	return (
-		<header className={styles.header}>
-			<Link className={styles.logo} href='/'>
-				<Image priority src={logoImg} alt='A plate with food on it' />
-				NextLevel Food
-			</Link>
-			<nav className={styles.nav}>
-				<ul>
-					<li>
-						<Link href='/meals'>Browse Meals</Link>
-					</li>
-					<li>
-						<Link href='/community'>Foodie Community</Link>
-					</li>
-				</ul>
-			</nav>
-		</header>
+		<>
+			<MainHeaderBackground />
+			<header className={styles.header}>
+				<Link className={styles.logo} href='/'>
+					<Image priority src={logoImg} alt='A plate with food on it' />
+					NextLevel Food
+				</Link>
+				<nav className={styles.nav}>
+					<ul>
+						<li>
+							<Link href='/meals'>Browse Meals</Link>
+						</li>
+						<li>
+							<Link href='/community'>Foodie Community</Link>
+						</li>
+					</ul>
+				</nav>
+			</header>
+		</>
 	);
 };
 export default MainHeader;
