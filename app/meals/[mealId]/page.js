69c448ecc789fc8654404663084647afdbf31729
Sergii Burgazlieiev <frontenddevsb@gmail.com>
2024-02-13 11:37:45 -0600

    starter project

---
+++ app/meals/[mealId]/page.js
@@ -0,0 +1,9 @@
+const MealDetailPage = () => {
+	return (
+		<main>
+			<h1>Meal detail Page</h1>
+		</main>
+	);
+};
+
+export default MealDetailPage;
---
+++ app/meals/share/page.js
@@ -0,0 +1,9 @@
+const ShareMeals = () => {
+	return (
+		<main>
+			<h1>Share Meals</h1>
+		</main>
+	);
+};
+
+export default ShareMeals;
